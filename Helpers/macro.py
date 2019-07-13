62c46fa0adc5ec792848b4b63df893305399a616
Maximilien Rigaut <max.rigaut@gmail.com>
2019-07-13 18:27:35 +0200

    Added most of the code of the macro generator

--- Helpers/macro.py
+++ Helpers/macro.py
@@ -1,5 +1,5 @@
 import sys,os,traceback
-import re,argparse
+import re,argparse,configparser
 from os import listdir
 from os.path import isfile, join
 
@@ -7,7 +7,7 @@ import blessings
 term=blessings.Terminal()
 
 parser = argparse.ArgumentParser(description='Macro generator')
-parser.add_argument('command',choices=["macro","varnames","diff"],help="The selected command")
+parser.add_argument('command',choices=["macro","varnames","diff",'show'],help="The selected command")
 parser.add_argument('characters',default=["all"],help="Name of the characters to list, all by default",nargs="*")
 parser.add_argument("-o",metavar="output",default="tty",help="File for the output command, tty by default",nargs="?")
 parser.add_argument("-v",default=False,action='store_true',help="Default")
@@ -19,12 +19,42 @@ def dprint(text):
 
 reserved_chars=["exemple.ini","God.ini"]
 errors={
-    "notfound":term.red("Couldn't find {char} in the Character directory")
+    "notfound":term.red("Couldn't find {char} in the Character directory"),
+    "sectionnotfound":term.red("Coulnd't find section {section} in {where}")
 }
 
 
 def macro(e):
     pass
+
+def show(characters,output,version):
+    list_files=get_char(characters)
+    if not list_files:
+        return
+    ret=""
+    for file in list_files:
+        ret+=show_one(file,output,version)
+    if output=="tty":
+        print(ret)
+    else:
+        with open(output,"r") as f:
+            f.write(ret)
+
+def show_one(character,output,version):
+    config=configparser.ConfigParser()
+    config.read(character)
+    ret=""
+    for sect in config.sections():
+        text=""
+        text+=term.bold_green("[{level}] {title}\n").format(title=config[sect]["display_name"],level=config[sect]["level"])
+        for dname,sname in (('Description',"description"),("Description personelle",'self'),("Description publique","others"),("Spécial","special")):
+            try:
+                adden=" {t}\n   {c}\n".format(t=term.green_underline(dname),c=config[sect][sname].replace("\n","\n   "))
+                text+=adden
+            except:
+                dprint(errors["sectionnotfound"].format(section=sname,where=sect))
+        ret+=text
+    return ret
 
 def varnames(e):
     pass
@@ -69,5 +99,5 @@ if __name__ == '__main__':
         parser.print_help()
         sys.exit()
     #print(args)
-    commands={"macro":macro,"varnames":varnames,"diff":diff}
+    commands={"macro":macro,"varnames":varnames,"diff":diff,"show":show}
     commands[args.command](args.characters,args.o,args.version)

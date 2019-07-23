a645f110a8213232e64da55764fcd1c02c633aea
Maximilien Rigaut <max.rigaut@gmail.com>
2019-07-23 23:24:57 +0200

    All macros

--- Helpers/push.sh
+++ Helpers/push.sh
@@ -1,6 +1,7 @@
 python3 macro.py -v show -o Generated
 python3 macro.py -v macro -o Generated
 python3 macro.py -v index -o Changes
+python3 macro.py -v index -o Generated
 #python3 macro.py -v diff -v ~-1 -o Changes
 git add -A
 git commit
